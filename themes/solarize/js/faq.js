7a786e224af27f4b828d4e657aa562ad4123033b
benjaminm278 <54561534+benjaminm278@users.noreply.github.com>
2026-02-12 19:31:58 -0500

    feat: Automatic question closure when opening another question

--- themes/solarize/js/faq.js
+++ themes/solarize/js/faq.js
@@ -1,15 +1,33 @@
 $(document).ready(function() {
     $(".answer-sect").hide();
+    let current = null;
     $(".question-header").click(function() {
-        $(this).next(".answer-sect").slideToggle(300);
+        // Close question currently open
+        if (current) {
+            current.find("i").css({
+                transform: ""
+            });
+            current.next(".answer-sect").slideToggle(300);
+        }
+
+        // Open question clicked
         let first = $(this).find("i");
         let transformValue = "";
+
+        // Slide answer section open/close
+        $(this).next(".answer-sect").slideToggle(300);
+
+        // Flip arrow to open state if answer is not showing
         if (first.css("transform") == "none") {
             transformValue = "rotate(180deg)";
         }
 
+        // Rotate arrow
         first.css({
             transform: transformValue
         });
+
+        // Track current question
+        current = $(this);
     })
 });

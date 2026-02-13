cc1cb97e6a5bfa59d682344bcda5872149910149
benjaminm278 <54561534+benjaminm278@users.noreply.github.com>
2026-02-12 19:22:15 -0500

    chore: Adjust pictures

--- themes/solarize/js/init.js
+++ themes/solarize/js/init.js
@@ -34,7 +34,7 @@
 				},
 				titleBar: {
 					breakpoints: 'narrower',
-					height: 44,
+					height: 52,
 					html: '<span class="toggle" data-action="toggleLayer" data-args="navPanel"></span><span class="title" data-action="copyHTML" data-args="logo"></span>',
 					position: 'top-left',
 					side: 'top',
